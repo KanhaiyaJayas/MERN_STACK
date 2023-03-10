09a86619326544f3189640aefc9c1c9f957dfcf5
jayaskanhaiya0001 <jayaskanhaiya0001@gmail.com>
2023-03-10 22:03:30 +0530

    Get Api

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 3000;
-app.get('/' , (req , res) => {
-    res.status(200).json({message: 'hello from the server side!' , name: "kanhaiya" , age: 21});
-});
 
-app.post('/' , (req , res) => {
+
+
+// app.get('/' , (req , res) => {
+//     res.status(200).json({message: 'hello from the server side!' , name: "kanhaiya" , age: 21});
+// });
+
+// app.post('/' , (req , res) => {
+//     res.status(200).json({
+//         name: "kanhaiya",
+//         age: 21
+//     })
+// })
+
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/tours-simple.json`));
+
+// I use JSON.parse befor reading because this data in json format and i wanna data in Java script object foermat so i use json.parse
+
+
+app.get('/api/v1/tours' , (req , res) => {
     res.status(200).json({
-        name: "kanhaiya",
-        age: 21
+        status: 'success',
+        dataLength: tours.length,
+        data: {
+            tours: tours
+        }
     })
 })
+
+
+
 app.listen(port , () => {
     console.log(`App runing on port ${port}...`);
 });
