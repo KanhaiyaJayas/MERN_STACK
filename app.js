5a41d27b920f8c0c0df0e20960de356ae9c1149e
jayaskanhaiya0001 <jayaskanhaiya0001@gmail.com>
2023-03-11 10:28:02 +0530

    Delete Tour

--- app.js
+++ app.js
@@ -86,6 +86,19 @@ app.patch('/api/v1/tours/:id' , (req , res) => {
    }) 
 })
 
+app.delete('/api/v1/tours/:id' , (req , res) => {
+    if(req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Write Correct Id'
+        })
+    }
+   res.status(200).json({
+    status: 'success',
+    data: null
+   }) 
+})
+
 app.listen(port , () => {
     console.log(`App runing on port ${port}...`);
 });
