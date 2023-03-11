b3840cf2b865d1b1e5f9a6f5bb66b68e0dcbf527
jayaskanhaiya0001 <jayaskanhaiya0001@gmail.com>
2023-03-11 16:02:54 +0530

    Params middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,8 +2,15 @@ const fs = require('fs');
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/../dev-data/tours-simple.json`)
 );
-
-
+exports.checkID = (req , res , next , val) => {
+  if (req.params.id * 1 > tours.length) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id'
+    })
+  }
+  next();
+}
 exports.getAllTours = (req, res) => {
   // console.log(requestTime)
   res.status(200).json({
@@ -19,12 +26,6 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
   let id = req.params.id * 1;
   const tour = tours.find((data) => data.id == id);
-  if (!tour) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Write Correct Id",
-    });
-  }
   res.status(200).json({
     status: "success",
     data: {
@@ -52,12 +53,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Write Correct Id",
-    });
-  }
   res.status(200).json({
     status: "success",
     data: {
@@ -67,12 +62,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Write Correct Id",
-    });
-  }
   res.status(200).json({
     status: "success",
     data: null,
--- routes/tour-routes.js
+++ routes/tour-routes.js
@@ -1,7 +1,8 @@
-const fs = require('fs');
 const tourController = require('../controllers/tourController');
 const express = require("express");
 const router = express.Router();
+
+router.param('id' , tourController.checkID)
 router.route("/").get(tourController.getAllTours).post(tourController.createTour);
 router.route("/:id").get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
 
