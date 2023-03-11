2e57b5ab0d39c3f8ede83a55153ff01fcadc5000
jayaskanhaiya0001 <jayaskanhaiya0001@gmail.com>
2023-03-11 10:09:41 +0530

    Get Data By Parameter

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ app.get('/api/v1/tours' , (req , res) => {
 })
 
 app.post('/api/v1/tours' , (req , res) => {
-    // console.log(req.body);
     const newId = tours[tours.length - 1].id + 1;
     const newTour = Object.assign({id: newId}, req.body);
     tours.push(newTour);
@@ -46,6 +45,28 @@ app.post('/api/v1/tours' , (req , res) => {
         })
     })
 })
+
+
+
+app.get('/api/v1/tours/:id' , (req, res) => {
+    let id = req.params.id * 1;
+    const tour = tours.find((data) => data.id == id);
+    if(!tour) {
+       return res.status(400).json({
+            status: 'fail',
+            message: "Write Correct Id"
+        })
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: tour
+        }
+    })
+})
+
+//Get Data By Params
+
 
 app.listen(port , () => {
     console.log(`App runing on port ${port}...`);
