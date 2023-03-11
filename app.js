16de80073010bb996d027c7fb9f242000c729ef3
jayaskanhaiya0001 <jayaskanhaiya0001@gmail.com>
2023-03-11 14:28:21 +0530

    File Structure

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const fs = require("fs");
 const express = require("express");
 const morgan = require("morgan");
+
+const tourRouter = require("./routes/tour-routes");
+const userRouter = require("./routes/user-routes");
 
 const app = express();
 
@@ -17,137 +19,13 @@ app.use((req, res, next) => {
 //     req.requestTime = new Date().toISOString();
 //     next();
 // })
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/tours-simple.json`)
-);
 
 // I use JSON.parse befor reading because this data in json format and i wanna data in Java script object foermat so i use json.parse
 
 // 2) ROUTE HANDLER
-const getAllTours = (req, res) => {
-  // console.log(requestTime)
-  res.status(200).json({
-    status: "success",
-    dataLength: tours.length,
-    // requestedAt: req.requestTime,
-    data: {
-      tours: tours,
-    },
-  });
-};
 
-const getTour = (req, res) => {
-  let id = req.params.id * 1;
-  const tour = tours.find((data) => data.id == id);
-  if (!tour) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Write Correct Id",
-    });
-  }
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour: tour,
-    },
-  });
-};
-
-const createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: "success",
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-};
-
-const updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Write Correct Id",
-    });
-  }
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour: "<Updated tour here...>",
-    },
-  });
-};
-
-const deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Write Correct Id",
-    });
-  }
-  res.status(200).json({
-    status: "success",
-    data: null,
-  });
-};
-
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: "Error",
-    message: "This route is not yet defined",
-  });
-};
-
-const getUser = (req, res) => {
-  res.status(500).json({
-    status: "Error",
-    message: "This route is not yet defined",
-  });
-};
-
-const createUser = (req, res) => {
-  res.status(500).json({
-    status: "Error",
-    message: "This route is not yet defined",
-  });
-};
-
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: "Error",
-    message: "This route is not yet defined",
-  });
-};
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: "Error",
-    message: "This route is not yet defined",
-  });
-};
 // 3) ROUTES
-
-const tourRouter = express.Router();
-const userRouter = express.Router();
-tourRouter.route("/").get(getAllTours).post(createTour);
-
-tourRouter.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
-
-userRouter.route("/").get(getAllUsers).post(createUser);
-
-userRouter.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
-
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
-// 4) SERVER
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App runing on port ${port}...`);
-});
+
+module.exports = app;
