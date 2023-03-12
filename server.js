564be3c91037a5cdf8c740a14e899d89908d0e4f
jayaskanhaiya0001 <jayaskanhaiya0001@gmail.com>
2023-03-12 11:54:22 +0530

    Creating Documents/Schemas and Testing Model

--- server.js
+++ server.js
@@ -14,6 +14,34 @@ mongoose.connect("mongodb://localhost:27017/CRUD", {
   console.log(err )
 });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name']
+  },
+  rating: {
+    type: Number,
+    default: true,
+    unique: true
+  },
+  price: {
+    type: Number,
+    required: [true , 'A tour must have a price']
+  }
+});
+const Tour = mongoose.model('Tour' , tourSchema);
+
+const newTour = new Tour({
+  name: "Helicopter",
+  rating: 4.5,
+  price: 100000
+});
+
+newTour.save().then((data) => {
+  console.log(data)
+}).catch((err) => {
+  console.log('Err')
+})
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
